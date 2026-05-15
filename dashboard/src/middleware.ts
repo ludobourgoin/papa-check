1065332e8381fe580ddf8c4fd99d27da1c680940
ludo-coolbeans <ludo@coolbeans.cc>
2026-05-15 21:26:30 +0200

    Remove middleware entirely to isolate [object Object] bug

--- dashboard/src/middleware.ts
+++
@@ -1,5 +0,0 @@
-// Pass-through middleware pour diagnostiquer le bug [object Object].
-// On réintroduira l'auth Clerk après avoir confirmé qu'Astro rend correctement.
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async (_context, next) => next());
